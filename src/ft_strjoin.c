85c1a1e7749a91b857f6081db610989aab5c07ae
koriori <maaacha.kuri05@gmail.com>
2023-02-01 09:56:29 +0900

    fix input NULL

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ihashimo <maaacha.kuri05@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 23:50:07 by ihashimo          #+#    #+#             */
-/*   Updated: 2022/12/19 20:04:04 by ihashimo         ###   ########.fr       */
+/*   Updated: 2023/02/01 09:55:34 by ihashimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,26 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	i;
 	size_t	j;
+	size_t	len;
 	char	*ret;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2)
+		s1 = "";
+	else if (s1 && s2 == NULL)
+		s2 = "";
+	else if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	ret = (char *)ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
 	if (!ret)
 		return (NULL);
+	len = 0;
 	i = 0;
 	while (s1[i] != '\0')
-	{
-		ret[i] = s1[i];
-		i++;
-	}
+		ret[len++] = s1[i++];
 	j = 0;
 	while (s2[j] != '\0')
-	{
-		ret[i + j] = s2[j];
-		j++;
-	}
-	ret[i + j] = '\0';
+		ret[len++] = s2[j++];
+	ret[len] = '\0';
 	return (ret);
 }
 /*
